094222e55b380db92b956e8a45e8ffdba355afee
Azhar Khan <azharkhan844716@gmail.com>
2026-02-02 10:34:55 +0530

    Header section 2206

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,12 +11,20 @@ export const metadata = {
 };
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <Header/>
+        <main className="min-h-screen">{children}</main>
+        <footer className="py-8 px-4 border-t border-stone-200">
+          <div className=" max-w-6xl mx-auto flex justify-center items-center">
+            <p className="text-stone-500 text-sm">
+              Made with 💗 by RoadsideCoder
+            </p>
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
