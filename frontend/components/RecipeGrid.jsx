ffec8a9d42dde31660376dc0423838443ad29619
Azhar Khan <azharkhan844716@gmail.com>
2026-02-06 03:27:09 +0530

    Dashboard page Done 20228

---
+++ frontend/components/RecipeGrid.jsx
@@ -0,0 +1,9 @@
+export default function RecipeGrid(){
+    return(
+       <div className="min-h-screen px-4 pb-16 pt-14">nn
+        <div className="container mx-auto max-w-7xl  ">kn
+            <div className="mb-8">LInk</div>
+        </div>
+       </div>
+    )
+}
