954fa0f1b81b25a9317311bfc02d19b06ea1d661
Azhar Khan <azharkhan844716@gmail.com>
2026-05-27 01:40:11 +0530

    middleware  updated

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://servd-pi.vercel.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
+
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
